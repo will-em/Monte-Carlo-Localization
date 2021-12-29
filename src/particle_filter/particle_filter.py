946b14ba75baca913f29ed96dc67ae8d9c807183
Eirini <ir.stratigi@gmail.com>
2021-12-29 18:25:42 +0100

    Update particle_filter.py

--- src/particle_filter/particle_filter.py
+++ src/particle_filter/particle_filter.py
@@ -1,12 +1,21 @@
 import numpy as np
+import measurement_model as mm
+import data_association as data
 
 class particle_filter:
 
     def __init__(self, num_of_particles, landmarks):
         self.num_of_particles = num_of_particles
         self.M = len(landmarks)
+        self.map = landmarks               # x y z yaw pitch roll
+        self.boundaries = [0, 100, 0, 100] # [x_start, x_end, z_start, z_end]
+        self.particles = np.zeros((6, num_of_particles))
+    
+        for i in range(num_of_particles):
+            self.particles[:, i] = np.array()
 
-    def step(obv):
+    def step(self, obv):
+        
         pass
 
 
