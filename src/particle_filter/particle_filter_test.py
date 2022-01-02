f97e1c9a8e4bbe60bfe7836c47d3cc6fd56a8a4e
William Emanuelsson <wem@kth.se>
2022-01-02 21:16:51 +0100

    Fix

--- src/particle_filter/particle_filter_test.py
+++ src/particle_filter/particle_filter_test.py
@@ -1,5 +1,6 @@
-from Particle_filter import Particle_filter
+from particle_filter import Particle_filter
 from aruco_detect import read_image, test_image
+from ..read_landmarks import read_landmarks
 import numpy as np
 import cv2
 
@@ -12,6 +13,7 @@ import matplotlib.pyplot as plt
 if __name__=="__main__":
     W_1 = np.array([(0, 0, 4, 0, np.pi, 0)]) #right marker (double_marker.png)
     W_2 = np.array([(4, 0, 0, 0, -np.pi/2, 0)]) #left marker (double_marker.png)
+
     W = np.concatenate((W_1.T, W_2.T), axis=1)
 
     Q = 3.0 * np.eye(12)
@@ -37,30 +39,30 @@ if __name__=="__main__":
 
         if ret is False:
             break
+        if i % 2 == 0:
+            if i > 80:
+                z_t = read_image(img)
+                #cv2.imshow(img)
+                fig = plt.figure()
+                plt.scatter(PF.particles[0,:],PF.particles[2,:], alpha=0.03)
+                plt.scatter(np.mean(PF.particles[0,:]),np.mean(PF.particles[2,:]))
 
-        if i > 80:
-            z_t = read_image(img)
-            #cv2.imshow(img)
-            fig = plt.figure()
-            plt.scatter(PF.particles[0,:],PF.particles[2,:], alpha=0.03)
-            plt.scatter(np.mean(PF.particles[0,:]),np.mean(PF.particles[2,:]))
+                fig.canvas.draw()
 
-            fig.canvas.draw()
+                minimap = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8,
+                        sep='')
+                minimap  = minimap.reshape(fig.canvas.get_width_height()[::-1] + (3,))
 
-            minimap = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8,
-                    sep='')
-            minimap  = minimap.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+                # img is rgb, convert to opencv's default bgr
+                minimap = cv2.cvtColor(minimap,cv2.COLOR_RGB2BGR)
 
-            # img is rgb, convert to opencv's default bgr
-            minimap = cv2.cvtColor(minimap,cv2.COLOR_RGB2BGR)
+                cv2.imshow('Minimap', minimap)
 
-            cv2.imshow('Minimap', minimap)
+                cv2.imshow('Game', img) #shows game
 
-            cv2.imshow('Game', img) #shows game
+                key = cv2.waitKey(33) & 0xFF
+                if key == 27:
+                    break
 
-            key = cv2.waitKey(33) & 0xFF
-            if key == 27:
-                break
-
-            PF.step(z_t)
-            #PF.plot()
+                PF.step(z_t)
+                #PF.plot()
